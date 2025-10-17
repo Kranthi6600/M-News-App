089869062da5ed4f3c246153ef5fd82fc2c5a936
Kranthi Chintu <kranthichintu6600@gmail.com>
2025-10-17 12:23:27 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import News from './components/News';
 import {
-  BrowserRouter as Router,
+  HashRouter  as Router,
   Route,
   Routes,
 } from "react-router-dom";
